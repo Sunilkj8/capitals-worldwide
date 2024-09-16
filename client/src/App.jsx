90e7bc198f852ef8ddef60dc3d2ca7549cabeeeb
Sunilkj8 <sunil2003jakhar@gmail.com>
2024-09-16 17:24:38 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import {Form,countryChange} from "./form";
+import Score from "./score";
+
+ 
+// console.log(generateCountry());
+ 
+function App() {
+     return (
+    <Form />
+
+  );
+}
+export default App;
